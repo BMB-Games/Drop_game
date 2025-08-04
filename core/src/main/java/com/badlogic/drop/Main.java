c4ca055b940949b13ec89f7d8618dc0344073218
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2025-08-04 13:44:51 +0200

    Implement bucket sprite movement and refactor drawing logic

--- core/src/main/java/com/badlogic/drop/Main.java
+++ core/src/main/java/com/badlogic/drop/Main.java
@@ -2,10 +2,12 @@ package com.badlogic.drop;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -21,6 +23,7 @@ public class Main implements ApplicationListener {
     private FitViewport viewport;
     private Float worldWidth = 8f;
     private Float worldHeight = 5f;
+    private Sprite bucketSprite;
 
     @Override
     public void create() {
@@ -31,6 +34,8 @@ public class Main implements ApplicationListener {
         music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
         batch = new SpriteBatch();
         viewport = new FitViewport(worldWidth, worldHeight);
+        bucketSprite = new Sprite(bucketTexture);
+        bucketSprite.setSize(1,1);
     }
 
     @Override
@@ -52,7 +57,7 @@ public class Main implements ApplicationListener {
         batch.begin();
 
         batch.draw(backgroundTexture, 0,0, worldWidth, worldHeight);
-        batch.draw(bucketTexture, 0, 0, 1, 1);
+        bucketSprite.draw(batch);
 
 
         batch.end();
@@ -63,6 +68,14 @@ public class Main implements ApplicationListener {
     }
 
     private void input() {
+        float speed = 10f;
+        float delta = Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            bucketSprite.translateX(speed * delta);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            bucketSprite.translateX(-speed * delta);
+        }
+
 
     }
 
