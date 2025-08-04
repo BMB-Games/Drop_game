20172fa2d92cb6d6686f5232feb7869f02e8c0a3
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2025-08-04 12:56:12 +0200

    Refactor Main class to enhance rendering and audio management

--- core/src/main/java/com/badlogic/drop/Main.java
+++ core/src/main/java/com/badlogic/drop/Main.java
@@ -4,37 +4,66 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+
 public class Main implements ApplicationListener {
     private Texture backgroundTexture;
     private Texture bucketTexture;
     private Texture dropTexture;
     private Sound dropSound;
     private Music music;
-
+    private SpriteBatch batch;
+    private FitViewport viewport;
+    private Float worldWidth = 8f;
+    private Float worldHeight = 5f;
 
     @Override
     public void create() {
         backgroundTexture = new Texture("background.png");
         bucketTexture = new Texture("bucket.png");
         dropTexture = new Texture("drop.png");
-        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
-        music = Gdx.audio.newMusic(Gdx.files.internal("drop.wav"));
-
-
-
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        batch = new SpriteBatch();
+        viewport = new FitViewport(worldWidth, worldHeight);
     }
 
     @Override
     public void resize(int width, int height) {
-        // Resize your application here. The parameters represent the new window size.
+        viewport.update(width, height, true);
     }
 
     @Override
     public void render() {
-        // Draw your application here.
+        input();
+        logic();
+        draw();
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+        batch.begin();
+
+        batch.draw(backgroundTexture, 0,0, worldWidth, worldHeight);
+        batch.draw(bucketTexture, 0, 0, 1, 1);
+
+
+        batch.end();
+    }
+
+    private void logic() {
+
+    }
+
+    private void input() {
+
     }
 
     @Override
