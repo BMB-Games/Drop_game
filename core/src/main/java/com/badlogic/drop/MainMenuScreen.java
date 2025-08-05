cd98bbd48a0575948c4d42d957635e9d0fd446bd
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2025-08-05 18:17:49 +0200

    FEAT MainMenuScreen

--- core/src/main/java/com/badlogic/drop/MainMenuScreen.java
+++ core/src/main/java/com/badlogic/drop/MainMenuScreen.java
@@ -1,5 +1,6 @@
 package com.badlogic.drop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -12,7 +13,6 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void show() {
-
     }
 
     @Override
@@ -25,6 +25,11 @@ public class MainMenuScreen implements Screen {
         game.font.draw(game.batch, "Welcome to drop!", 1, 1.5f);
         game.font.draw(game.batch, "Tap anywhere to start",  1, 1.5f);
         game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
     }
 
     @Override
@@ -49,6 +54,5 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
-
     }
 }
