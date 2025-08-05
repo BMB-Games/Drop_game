1617e956559b50b1f2881fdd207dd793639c0422
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2025-08-05 18:01:18 +0200

    Add Drop game class and MainMenuScreen for initial game setup

---
+++ core/src/main/java/com/badlogic/drop/Drop.java
@@ -0,0 +1,34 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+public class Drop extends Game {
+    SpriteBatch batch;
+    BitmapFont font;
+    FitViewport viewport;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        viewport = new FitViewport(8, 5);
+
+        font.setUseIntegerPositions(false);
+        font.getData().setScale(viewport.getWorldWidth() / viewport.getWorldHeight()); // Scale font to fit the viewport
+        this.setScreen(new MainMenuScreen(this));
+
+    }
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+}
---
+++ core/src/main/java/com/badlogic/drop/MainMenuScreen.java
@@ -0,0 +1,54 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MainMenuScreen implements Screen {
+    private Drop game;
+    public MainMenuScreen(Drop game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(Color.BLACK);
+        game.viewport.apply();
+        game.batch.setProjectionMatrix(game.viewport.getCamera().combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to drop!", 1, 1.5f);
+        game.font.draw(game.batch, "Tap anywhere to start",  1, 1.5f);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
